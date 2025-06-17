2488e356332811cf9fd4e7adc85338bccb6be42a
jinwook song <wlsdnr129@naver.com>
2025-06-17 18:37:21 +0900

    feat: scrape jobs

--- learn-go/go.mod
+++ learn-go/go.mod
@@ -7,3 +7,9 @@ replace github.com/jinwook-song/learn-go/banking => ./banking
 replace github.com/jinwook-song/learn-go/my_dict => ./my_dict
 
 replace github.com/jinwook-song/learn-go/urlchecker => ./urlchecker
+
+require (
+	github.com/PuerkitoBio/goquery v1.10.3 // indirect
+	github.com/andybalholm/cascadia v1.3.3 // indirect
+	golang.org/x/net v0.39.0 // indirect
+)
--- learn-go/main.go
+++ learn-go/main.go
@@ -1,49 +1,124 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
-	"sync"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
 
-	"github.com/jinwook-song/learn-go/urlchecker"
+	"github.com/PuerkitoBio/goquery"
 )
 
+type extractedJob struct {
+	id       string
+	title    string
+	location string
+}
+
+const searchWord string = "python"
+const baseUrl string = "https://www.saramin.co.kr/zf_user/search/recruit?recruitPageCount=100&recruitPage=1&searchword=" + searchWord
+const fileName string = "jobs/" + searchWord + ".csv"
+
 func main() {
-	urls := []string{
-		"https://www.google.com",
-		"https://www.facebook.com",
-		"https://www.twitter.com",
+	var jobs []extractedJob
+	totalPages := getPages()
+	for i := 1; i <= totalPages; i++ {
+		jobs = append(jobs, getPage(i)...)
 	}
 
-	// 버퍼드 채널 생성
-	resultChan := make(chan struct{ url, status string }, len(urls))
-	var wg sync.WaitGroup
+	writeJobs(jobs)
+	fmt.Println("Done, extracted", len(jobs))
+}
 
-	for _, url := range urls {
-		wg.Add(1)
-		go func(url string) {
-			defer wg.Done()
-			status := "OK"
-			if err := urlchecker.HitURL(url); err != nil {
-				status = "FAILED"
+func writeJobs(jobs []extractedJob) {
+	file, err := os.Create(fileName)
+	checkErr(err)
+	defer file.Close()
+
+	w := csv.NewWriter(file)
+	defer w.Flush()
+
+	headers := []string{"id", "title", "location"}
+	headerErr := w.Write(headers)
+	checkErr(headerErr)
+
+	for _, job := range jobs {
+		csvLine := []string{job.id, job.title, job.location}
+		csvError := w.Write(csvLine)
+		checkErr(csvError)
+	}
+}
+
+func getPage(page int) []extractedJob {
+	pageUrl := baseUrl + "&recruitPage=" + strconv.Itoa(page)
+	res, err := http.Get(pageUrl)
+	checkErr(err)
+	checkResponseCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	jobs := extractJobs(doc)
+	return jobs
+}
+
+func extractJobs(doc *goquery.Document) []extractedJob {
+	jobs := []extractedJob{}
+	doc.Find(".item_recruit").Each(func(i int, s *goquery.Selection) {
+		id, _ := s.Attr("value")
+		title := cleanString(s.Find(".area_job").Find(".job_tit").Find("a").Text())
+		location := cleanString(s.Find(".area_job").Find(".job_condition>span>a").Text())
+
+		job := extractedJob{
+			id:       id,
+			title:    title,
+			location: location,
+		}
+		jobs = append(jobs, job)
+	})
+	return jobs
+}
+
+func cleanString(str string) string {
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
+}
+
+func getPages() int {
+	pages := 1
+	res, err := http.Get(baseUrl)
+
+	checkErr(err)
+	checkResponseCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
+		s.Find("a").Each(func(i int, a *goquery.Selection) {
+			if !strings.Contains(a.Text(), "다음") {
+				pages++
 			}
-			resultChan <- struct{ url, status string }{url, status}
-		}(url)
+		})
+	})
+
+	return pages
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
 	}
+}
 
-	// 모든 Go 루틴 완료 대기
-	wg.Wait()
-	// 채널 닫기
-	close(resultChan)
-
-	// 메인 Go 루틴에서 결과 수집
-	results := make(map[string]string)
-	for res := range resultChan {
-		// <-resultChan으로 받은 데이터를 results 맵에 저장
-		results[res.url] = res.status
-	}
-
-	// 결과 출력
-	for url, result := range results {
-		fmt.Printf("%s: %s\n", url, result)
+func checkResponseCode(res *http.Response) {
+	if res.StatusCode != 200 {
+		log.Fatal("Request failed with Status:", res.StatusCode)
 	}
 }
